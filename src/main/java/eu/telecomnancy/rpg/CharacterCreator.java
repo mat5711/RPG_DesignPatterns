8a904d41c26c1aefd005be6f01ee67b68b5c7750
Mathieu Breit <mathieu.breit@telecomnancy.eu>
2024-11-18 09:56:23 +0100

    ajout de la classe GameConfiguration et de l'interface CharacterCreator

---
+++ src/main/java/eu/telecomnancy/rpg/CharacterCreator.java
@@ -0,0 +1,5 @@
+package main.java.eu.telecomnancy.rpg;
+
+public interface CharacterCreator {
+    public GameCharacter createCharacter();
+}
---
+++ src/main/java/eu/telecomnancy/rpg/GameConfiguration.java
@@ -0,0 +1,31 @@
+package main.java.eu.telecomnancy.rpg;
+
+public class GameConfiguration {
+    private static GameConfiguration instance;
+    int difficulty;
+    int sizeMax;
+
+    private GameConfiguration() {
+        this.difficulty = 1;
+        this.sizeMax = 10;
+    }
+
+
+    public static GameConfiguration getInstance() {
+        if (instance == null) { //Attention : ne pas mettre ici this.instance car le instance est static donc on peut y accéder de n'importe où, donc il ne faut pas mettre this ici
+            instance = new GameConfiguration();
+        }
+        return instance;
+    }
+
+
+    //On construit les getter :
+    public int getDifficulty() {
+        return this.difficulty;
+    }
+
+    public int getSizeMax() {
+        return this.sizeMax;
+    }
+
+}
